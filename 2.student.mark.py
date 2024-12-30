5b01eaa9f48f454b5909c70d64f1b0cd1f720059
23BI14162 <thptchl020@gmail.com>
2024-12-30 14:22:23 +0700

    2.student.mark.py

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,74 +1,90 @@
 class Ultra:
     def __init__(self):
-        self.strudents=[]
-        self.courses=[]
-        self.marks={}
-class students:
-    def __init__(self,sid,name,dob):
-        self.sid=sid
-        self.name=name
-        self.dob=dob
+        self.students = []
+        self.courses = []
+        self.marks = {}
+
+    def inputNumStudent(self):
+        return int(input("Enter the number of students: "))
+
+    def inputNumCourse(self):
+        return int(input("Enter the number of courses: "))
+
+    def inputInfoStudent(self):
+        sid = input("Enter student's ID: ")
+        name = input("Enter student's name: ")
+        dob = input("Enter student's date of birth: ")
+        self.students.append(Student(sid, name, dob))
+
+    def inputInfoCourse(self):
+        cid = input("Enter course's ID: ")
+        name = input("Enter course's name: ")
+        self.courses.append(Course(cid, name))
+
+    def inputMarksCourse(self):
+        cid = input("Enter course ID for input: ")
+        course = next((c for c in self.courses if c.cid == cid), None)
+        if not course:
+            print("Course unknown.")
+            return
+        if cid not in self.marks:
+            self.marks[cid] = {}
+        for student in self.students:
+            try:
+                mark = float(input(f"Enter mark for student {student.sid}: "))
+                self.marks[cid][student.sid] = mark
+            except ValueError:
+                print("Invalid mark. Skipping this student.")
+
+    def outputCourseList(self):
+        print("Courses:")
+        for course in self.courses:
+            print(course)
+
+    def outputStudentList(self):
+        print("Students:")
+        for student in self.students:
+            print(student)
+
+    def outputStudentMarks(self):
+        cid = input("Enter course ID for output: ")
+        if cid not in self.marks:
+            print("Course unknown.")
+            return
+        print(f"Marks for course {cid}:")
+        for sid, mark in self.marks[cid].items():
+            print(f"Student's ID: {sid}, Mark: {mark}")
+
+
+class Student:
+    def __init__(self, sid, name, dob):
+        self.sid = sid
+        self.name = name
+        self.dob = dob
+
     def __str__(self):
-        return f"id:{self.sid},name:{self.name},dob:{self.dob}"
-class courses:
-    def __init__(self,cid,name):
-        self.cid=cid
-        self.name=name
+        return f"ID: {self.sid}, Name: {self.name}, DOB: {self.dob}"
+
+
+class Course:
+    def __init__(self, cid, name):
+        self.cid = cid
+        self.name = name
+
     def __str__(self):
-        return f"id:{self.cid},name:{self.name}"
-def inputNumStudent(self):
-    return int(input("enter the number of student"))
-def inputNumCourse(self):
-    return int(input("enter the number of courses"))
-def inputInfoStudent(self):
-    sid=input("enter student's id")
-    name=input("enter student's name")
-    dob=input("enter student's date of birth")
-    self.students.append((sid,name,dob))
-def inputInfoCourse(self):
-    cid=input("enter course's id")
-    name=input("enter course's name")
-    self.courses.append((cid,name))
-def inputMarksCourse(self):
-    cid = input("Enter course ID for input: ")    
-    checked = next((c for c in self.course if c.cid == cid), None)
-    if not checked:
-        print("Course unknown")
-        return
-    if cid not in self.marks:
-        self.marks[cid] = {}
-    for student in self.students:
-        try:
-            mark = float(input(f"Enter mark for student {student.sid}: "))
-            self.marks[cid][student.sid] = mark
-        except ValueError:
-            print("Invalid mark. Skipping this student.")
-def outputCourseList(self):
-    print("courses:")
-    for j in self.courses:
-        print(j)
-def outputStudentList(self):
-    print("students:")
-    for k in self.students:
-        print(k)
-def outputStudentMarks(self):
-    cid=input("enter course id for output")
-    if cid not in self.marks:
-        print("course unknown")
-        return
-    print(f"marks for course {cid}:")
-    for sid,mark in self.marks[cid].itens():
-        print(f"student's id:{sid},mark:{mark}")
+        return f"ID: {self.cid}, Name: {self.name}"
+
+
 if __name__ == "__main__":
-    Ultra = Ultra()
+    ultra = Ultra()
 
-    num_students = Ultra.input_num_students()
+    num_students = ultra.inputNumStudent()
     for _ in range(num_students):
-        Ultra.input_student_info()
+        ultra.inputInfoStudent()
 
-    num_courses = Ultra.input_num_courses()
+    num_courses = ultra.inputNumCourse()
     for _ in range(num_courses):
-        Ultra.input_course_info()
+        ultra.inputInfoCourse()
 
     while True:
         print("\nOptions:")
@@ -81,15 +97,14 @@ if __name__ == "__main__":
         choice = input("Choose an option: ")
 
         if choice == "1":
-            Ultra.inputMarksCourse()
+            ultra.inputMarksCourse()
         elif choice == "2":
-            Ultra.outputCourseList()
+            ultra.outputCourseList()
         elif choice == "3":
-            Ultra.outputStudentList()
+            ultra.outputStudentList()
         elif choice == "4":
-            Ultra.outputStudentMarks()
+            ultra.outputStudentMarks()
         elif choice == "5":
             break
         else:
             print("Invalid choice. Please try again.")
-
